c757f9f3dc90a3662a93c1f7542993585e6d5876
Michael Behrns-Miller [bitpost-gentoo] <m@thedigitalmachine.com>
2018-03-22 11:24:17 -0400

    Constrain min_pct

--- src/commands.c
+++ src/commands.c
@@ -555,7 +555,7 @@ static bool cmd_resize_tiling_width_height(I3_CMD, Con *current, const char *way
 
     LOG("current->percent before = %f\n", current->percent);
 
-    double min_pct = fabs(ppt / 100.0);
+    double min_pct = max(0.05,min(0.3,fabs(ppt / 100.0)));
 
     /* Grow */
     if (ppt > 0) {
